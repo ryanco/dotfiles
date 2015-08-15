b1623160c6f5c5f20a8194b321c7762f7411c1d1
Ryan Colwell <ryangcolwell@gmail.com>
2015-08-15 10:33:56 -0500

    adding scripts

--- setup.sh
+++ setup.sh
@@ -6,5 +6,13 @@ dir=~/dotfiles
 files="vimrc bash_rc bash_profile"
 
 for file in $files; do
+    echo "Linking $file"
     ln -sf $dir/$file ~/.$file
 done
+
+for f in scripts/*; do
+    echo "Making $f executable"
+    chmod +x $f
+done
+
+echo "Setup Complete"
